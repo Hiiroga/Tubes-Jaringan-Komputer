8f2d9ba8ab3a2f3e7320e2226cac79593005b7dd
Yudhistira Maulana Samaratungga <yudhistirasamaratungga@gmail.com>
2025-05-17 08:49:53 +0700

    Update client.py

--- client.py
+++ client.py
@@ -11,15 +11,15 @@ def main():
     filename = sys.argv[3]
 
     try:
-        # Buat socket dan koneksi ke server
+     
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             client_socket.connect((server_host, server_port))
 
-            # Buat request GET
+            
             request = f"GET /{filename} HTTP/1.1\r\nHost: {server_host}\r\n\r\n"
             client_socket.sendall(request.encode())
 
-            # Terima respons dari server
+            
             response = b""
             while True:
                 data = client_socket.recv(1024)
